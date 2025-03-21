5185319654e34428c0a3dcd77d5adf41a291a4ed
starkers <starkycampbell@gmail.com>
2025-03-21 22:26:12 +1100

    fix!: Turns out high-numbered function key support is hit-and-miss in x11 and terminal emulators

--- keyboards/sofle/keymaps/starkers/keymap.c
+++ keyboards/sofle/keymaps/starkers/keymap.c
@@ -22,12 +22,12 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 /*
  * NORMAL
  * ,-----------------------------------------.                    ,-----------------------------------------.
- * |  `   |   1  |   2  |   3  |   4  |   5  |                    |   6  |   7  |   8  |   9  |   0  |  \   |
+ * |  `   |   1  |   2  |   3  |   4  |   5  |           Rotation |   6  |   7  |   8  |   9  |   0  |  \   |
+ * |------+------+------+------+------+------|           AltShF2  |------+------+------+------+------+------|
+ * | ESC  |   Q  |   W  |   E  |   R  |   T  |           AltShF3  |   Y  |   U  |   I  |   O  |   P  | Bspc |
  * |------+------+------+------+------+------|                    |------+------+------+------+------+------|
- * | ESC  |   Q  |   W  |   E  |   R  |   T  |                    |   Y  |   U  |   I  |   O  |   P  | Bspc |
- * |------+------+------+------+------+------|      Rot:  F17/F18 |------+------+------+------+------+------|
  * | Tab  |   A  |   S  |   D  |   F  |   G  |-------.    ,-------|   H  |   J  |   K  |   L  |   ;  |  '   |
- * |------+------+------+------+------+------|  MUTE |    |  F16  |------+------+------+------+------+------|
+ * |------+------+------+------+------+------|  MUTE |    |AltShF1|------+------+------+------+------+------|
  * |LShift|   Z  |   X  |   C  |   V  |   B  |-------|    |-------|   N  |   M  |   ,  |   .  |   /  |RShift|
  * `-----------------------------------------/       /     \      \-----------------------------------------'
  *            | LGUI | LAlt | LCTR |  [   | /Space  /       \Enter \  |   ]  | RCTR | RAlt | RGUI |
@@ -39,18 +39,18 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   KC_GRV,   KC_1,   KC_2,    KC_3,    KC_4,    KC_5,                     KC_6,    KC_7,    KC_8,    KC_9,    KC_0,  KC_BSLS,
   KC_ESC,   KC_Q,   KC_W,    KC_E,    KC_R,    KC_T,                     KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,  KC_BSPC,
   KC_TAB,   KC_A,   KC_S,    KC_D,    KC_F,    KC_G,                     KC_H,    KC_J,    KC_K,    KC_L, KC_SCLN,  KC_QUOT,
-  KC_LSFT,  KC_Z,   KC_X,    KC_C,    KC_V,    KC_B, KC_MUTE,    KC_F16,  KC_N,    KC_M, KC_COMM,  KC_DOT, KC_SLSH,  KC_RSFT,
+  KC_LSFT,  KC_Z,   KC_X,    KC_C,    KC_V,    KC_B, KC_MUTE,    A(S(KC_F1)),  KC_N,    KC_M, KC_COMM,  KC_DOT, KC_SLSH,  KC_RSFT,
             KC_LGUI,KC_LALT,KC_LCTL, LT(RAISED,KC_LBRC),KC_SPC,   KC_ENT, LT(2,KC_RBRC), KC_RCTL, KC_RALT, KC_RGUI
 ),
 /*
  * RAISED
  * ,-----------------------------------------.                    ,-----------------------------------------.
- * |  F12 |  F1  |  F2  |  F3  |  F4  |  F5  |                    |  F6  |  F7  |  F8  |  F9  |  F10 | F11  |
+ * |  F12 |  F1  |  F2  |  F3  |  F4  |  F5  |         Rotation:  |  F6  |  F7  |  F8  |  F9  |  F10 | F11  |
+ * |------+------+------+------+------+------|            AltShF5 |------+------+------+------+------+------|
+ * | xxx  |  INS | HOME | PGUP |  xxx |  xxx |            AltShF6 |  xxx | xxx  |  xxx |  xxx |  xxx |  DEL |
  * |------+------+------+------+------+------|                    |------+------+------+------+------+------|
- * | xxx  |  INS | HOME | PGUP |  xxx |  xxx |                    |  xxx | xxx  |  xxx |  xxx |  xxx |  DEL |
- * |------+------+------+------+------+------|        Rot: F14/F15|------+------+------+------+------+------|
  * | xxx  |  DEL | END  | PGDN |  xxx |  xxx |-------.    ,-------| LEFT | DOWN |  UP  |RIGHT |   -  |  =   |
- * |------+------+------+------+------+------|  xxx  |    |  F13  |------+------+------+------+------+------|
+ * |------+------+------+------+------+------|  xxx  |    |AltShF4|------+------+------+------+------+------|
  * | xxx  |  xxx | xxx  | xxx  |  xxx |  xxx |-------|    |-------|  xxx |  xxx |  xxx | xxx  |  xxx |  xxx |
  * `-----------------------------------------/       /     \      \-----------------------------------------'
  *            | xxx  | xxx  |  xxx |  xxx | /  xxx  /       \  xxx \  | xxx  |  xxx |  xxx |  xxx |
@@ -61,7 +61,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   KC_F12, KC_F1, KC_F2, KC_F3, KC_F4, KC_F5,                       KC_F6, KC_F7, KC_F8, KC_F9, KC_F10, KC_F11,
   KC_NO, KC_INS, KC_HOME, KC_PGUP, KC_NO, KC_NO,             KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_DEL,
   KC_NO, KC_DEL, KC_END, KC_PGDN, KC_NO, KC_NO,              KC_LEFT, KC_DOWN, KC_UP, KC_RGHT, KC_MINS, KC_EQL,
-  KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_F13, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO)
+  KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, A(S(KC_F4)), KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO)
 
 };
 
@@ -70,8 +70,8 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 //My thinking is that they should be software mapped, eg to next pane in tmux.conf.
 //F13 - F15 are used in RAISED as they are slightly more likely to already be in use (though still unlikely)
 const uint16_t PROGMEM encoder_map[][NUM_ENCODERS][NUM_DIRECTIONS] = {
-    [NORMAL] = { ENCODER_CCW_CW(KC_VOLD, KC_VOLU),  ENCODER_CCW_CW(KC_F17, KC_F18)  },
-    [RAISED] = { ENCODER_CCW_CW(KC_NO, KC_NO),  ENCODER_CCW_CW(KC_F14, KC_F15)  },
+    [NORMAL] = { ENCODER_CCW_CW(KC_VOLD, KC_VOLU),  ENCODER_CCW_CW(A(S(KC_F2)), A(S(KC_F3)))  },
+    [RAISED] = { ENCODER_CCW_CW(KC_NO, KC_NO),  ENCODER_CCW_CW(A(S(KC_F5)), A(S(KC_F6)))  },
 };
 #endif
 
