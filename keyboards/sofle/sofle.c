0cf3293324b976d8f21c65178fe12f48dc98690a
starkers <starkycampbell@gmail.com>
2025-03-23 19:55:12 +1100

    feat: more relevant oled messages

--- keyboards/sofle/sofle.c
+++ keyboards/sofle/sofle.c
@@ -66,36 +66,20 @@ static void render_logo(void) {
 
 void print_status_narrow(void) {
     oled_write_P(PSTR("\n\n"), false);
-    switch (get_highest_layer(layer_state)) {
-        case 0:
-            oled_write_ln_P(PSTR("Qwrt"), false);
-            break;
-        case 1:
-            oled_write_ln_P(PSTR("Clmk"), false);
-            break;
-        default:
-            oled_write_P(PSTR("Mod\n"), false);
-            break;
-    }
+    oled_write_ln_P(PSTR("Cam's keyboard"), false);
     oled_write_P(PSTR("\n\n"), false);
     oled_write_ln_P(PSTR("LAYER"), false);
     switch (get_highest_layer(layer_state)) {
         case 0:
+            oled_write_P(PSTR("Base"), false);
+            break;
         case 1:
-            oled_write_P(PSTR("Base\n"), false);
-            break;
-        case 2:
             oled_write_P(PSTR("Raise"), false);
-            break;
-        case 3:
-            oled_write_P(PSTR("Lower"), false);
             break;
         default:
             oled_write_ln_P(PSTR("Undef"), false);
     }
     oled_write_P(PSTR("\n\n"), false);
-    led_t led_usb_state = host_keyboard_led_state();
-    oled_write_ln_P(PSTR("CPSLK"), led_usb_state.caps_lock);
 }
 
 bool oled_task_kb(void) {
